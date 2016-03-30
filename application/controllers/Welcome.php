3c50283459617a0b3b17ef9a6d59a9882211390a
pyf <774133886@qq.com>
2016-03-30 20:25:33 +0800

    2016.3.30

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -22,71 +22,87 @@ class Welcome extends CI_Controller {
 	{
 		$this->load->view('welcome_message.html');
 	}
+	// 个人中心
     public function personalcenter()
 	{
 		$this->load->view('personalcenter.html');
 	}
-
+	// 点菜
 	function add(){
 
 		$this->load->view('order.html');
 	}
+	// 菜品详情
 	function info(){
 
 		$this->load->view('foodInfo.html');
 	}
+	// 个人中心
 	function personal(){
 
 		$this->load->view('personalcenter.html');
 	}
+	// 购物篮
 	function car(){
 
 		$this->load->view('shopcar.html');
 	}
+	// 登录
 	function login(){
 
 		$this->load->view('login.html');
 	}
+	// 注册
 	function register(){
 
 		$this->load->view('register.html');
 	}
+	// 宴会定制
 	function custom(){
 
 		$this->load->view('custom.html');
 	}
+	// 实时菜价
 	function price(){
 
 		$this->load->view('price.html');
 	}
+	// 我的收藏
 	function collection(){
 
 		$this->load->view('collection.html');
 	}
+	// 我的订单
 	function myorder(){
 
 		$this->load->view('myOrder.html');
 	}
+	// 我的退款
 	function back(){
 
 		$this->load->view('moneyBack.html');
 	}
+	// 我的余额
 	function balance(){
 
 		$this->load->view('balance.html');
 	}
+	// 会员卡
 	function member(){
 
 		$this->load->view('member.html');
 	}
+	// 优惠券
 	function coupon(){
 
 		$this->load->view('coupon.html');
 	}
+	// 收货地址
 	function address(){
 
 		$this->load->view('address.html');
 	}
+	//	点菜
 	function order(){
 
 		$this->load->view('order.html');
@@ -95,28 +111,41 @@ class Welcome extends CI_Controller {
 	function userInfo(){
 		$this->load->view('userInfo.html');
 	}
+	// 支付
 	function defray(){
 		$this->load->view('defray.html');
 	}
+	// 实时菜价
 	function priceChart(){
 		$this->load->view('priceChart.html');
 	}
-
+	// 购买成功
 	function succeed(){
 		$this->load->view('succeed.html');
 	}
-
+	// 忘记密码
 	function forget(){
 		$this->load->view('forgetpsw.html');
 	}
+	// 我的评价
 	function appraise(){
 		$this->load->view('appraise.html');
 	}
+	// 商品评价
 	function orderaps(){
 		$this->load->view('orderaps.html');
 	}
+	// 主页
 	function home(){
 		$this->load->view('home.html');
+	}
+	// 大厨推荐
+	function recommend(){
+		$this->load->view('recommend.html');
+	}
+	// 活动页面
+	function activity(){
+		$this->load->view('activity.html');
 	}
 
 
