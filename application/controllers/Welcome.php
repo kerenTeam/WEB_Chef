c7c4f7aba0918b658ed375c3a255f167c7e2b614
pyf <774133886@qq.com>
2016-05-16 17:35:30 +0800

    分块

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -7,188 +7,91 @@ class Welcome extends CI_Controller {
 	 * Index Page for this controller.
 	 *
 	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
+	 * 		http://example.com/index/welcome
 	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
+	 * 		http://example.com/index/welcome/index
 	 *	- or -
 	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
+	 * config/routes, it's displayed at http://example.com/
 	 *
 	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 * map to /index/welcome/<method_name>
+	 * @see https://codeigniter.com/user_guide/general/urls
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message.html');
+		$this->load->view('welcome_message');
 	}
 	// 个人中心
     public function personalcenter()
 	{
-		$this->load->view('personalcenter.html');
+		$this->load->view('personalcenter');
 	}
 	// 点菜
 	function add(){
 
-		$this->load->view('order.html');
+		$this->load->view('order');
+		$this->load->view('footer');
 	}
 	// 菜品详情
 	function info(){
 
-		$this->load->view('foodInfo.html');
-	}
-	// 个人中心
-	function personal(){
-
-		$this->load->view('personalcenter.html');
-	}
-	// 购物篮
-	function car(){
-
-		$this->load->view('shopcar.html');
-	}
-	// 登录
-	function login(){
-
-		$this->load->view('login.html');
-	}
-	// 注册
-	function register(){
-
-		$this->load->view('register.html');
-	}
-	// 宴会定制
-	function custom(){
-
-		$this->load->view('custom.html');
+		$this->load->view('foodInfo');
 	}
 	// 实时菜价
 	function price(){
 
-		$this->load->view('price.html');
+		$this->load->view('price');
 	}
 	// 我的收藏
 	function collection(){
 
-		$this->load->view('collection.html');
-	}
-	// 我的订单
-	function myorder(){
-
-		$this->load->view('myOrder.html');
-	}
-	// 我的退款
-	function back(){
-
-		$this->load->view('moneyBack.html');
+		$this->load->view('collection');
 	}
 	// 我的余额
 	function balance(){
 
-		$this->load->view('balance.html');
-	}
-	// 会员卡
-	function member(){
-
-		$this->load->view('member.html');
-	}
-	// 优惠券
-	function coupon(){
-
-		$this->load->view('coupon.html');
-	}
-	// 收货地址
-	function address(){
-
-		$this->load->view('address.html');
+		$this->load->view('balance');
 	}
 	//	点菜
 	function order(){
 
-		$this->load->view('order.html');
-	}
-	//个人信息
-	function userInfo(){
-		$this->load->view('userInfo.html');
-	}
-	// 支付
-	function defray(){
-		$this->load->view('defray.html');
+		$this->load->view('order');
 	}
 	// 实时菜价
 	function priceChart(){
-		$this->load->view('priceChart.html');
+		$this->load->view('priceChart');
 	}
-	// 购买成功
-	function succeed(){
-		$this->load->view('succeed.html');
-	}
-	// 忘记密码
-	function forget(){
-		$this->load->view('forgetpsw.html');
-	}
-	// 我的评价
-	function appraise(){
-		$this->load->view('appraise.html');
-	}
+	
 	// 商品评价
 	function orderaps(){
-		$this->load->view('orderaps.html');
+		$this->load->view('orderaps');
 	}
 	// 主页
 	function home(){
-		$this->load->view('home.html');
+		$this->load->view('home');
 	}
 	// 大厨推荐
 	function recommend(){
-		$this->load->view('recommend.html');
+		$this->load->view('recommend');
 	}
 	// 活动页面
 	function activity(){
-		$this->load->view('activity.html');
+		$this->load->view('activity');
+		$this->load->view('footer');
 	}
 	// 换一换
 	function exchange(){
-		$this->load->view('exchange.html');
+		$this->load->view('exchange');
 	}
 	// 搜索
 	function search(){
-		$this->load->view('search.html');
+		$this->load->view('search');
+		$this->load->view('footer');
 	}
 	// 帮助中心
 	function help(){
-		$this->load->view('help.html');
-	}
-	// 净菜
-	function vegetable(){
-		$this->load->view('vegetable.html');
-	}
-	// 服务
-	function service(){
-		$this->load->view('service.html');
-	}
-	// 服务详情
-	function lifeInfo2(){
-		$this->load->view('lifeInfo2.html');
-	}
-	// 伴餐
-	function elegance(){
-		$this->load->view('elegance.html');
-	}
-	// 庆典
-	function ceremonyType(){
-		$this->load->view('ceremonyType.html');
-	}
-	// 庆典详情
-	function Ceremony(){
-		$this->load->view('Ceremony.html');
-	}
-	// 客服
-	function customServ(){
-		$this->load->view('customServ.html');
-	}
-	// 宴会定制详情
-	function partyInfo(){
-		$this->load->view('partyInfo.html');
+		$this->load->view('help');
+		$this->load->view('footer');
 	}
 }
